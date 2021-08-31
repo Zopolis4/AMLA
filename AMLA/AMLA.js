77a1893fbc7dd1f0a664cff519b20257cbdb084f
Zopolis4 <64618338+Zopolis4@users.noreply.github.com>
2021-09-01 09:41:01 +1000

    Initial commit
    Creating the repository.

---
+++ AMLA/AMLA.js
@@ -0,0 +1,50 @@
+// ==UserScript==
+// @name	Automatic Mediafire Link Archiver
+// @namespace      Zopolis4
+// @description    Automatically archives all mediafire links on pages you visit by sending them to ArchiveTeam.
+// @version	1.0.0
+// @match	*
+// @run-at document-start
+// ==/UserScript==
+
+(function() {
+
+	document.addEventListener("DOMContentLoaded", function() {
+		if (location.hostname == "www.mediafire.com") {
+                        fetch("https://urls.ajay.app/api/submitURLs", {
+			  method: "POST",
+			  body: JSON.stringify({
+			    urls: [ location.href ]
+			  }),
+			  headers: {
+                            "Content-Type": "application/json"
+			  }
+			});
+			console.log("Sent " + location.href + " To https://urls.ajay.app/");
+		}
+	}, false);
+
+
+
+	window.addEventListener("load", function() {
+		for (var elements1 = document.getElementsByTagName("a"), i = elements1.length - 1; i >= 0; i--) {
+			if (location.hostname == "www.mediafire.com") {
+				return;
+			}
+			if (elements1[i].hostname == "www.mediafire.com" || elements1[i].hostname == "www.mfi.re") {
+				var adrs2 = "http://example.com" + elements1[i].href;
+				fetch("https://urls.ajay.app/api/submitURLs", {
+				  method: "POST",
+				  body: JSON.stringify({
+				    urls: [ elements1[i].href ]
+				  }),
+				  headers: {
+				    "Content-Type": "application/json"
+				  }
+				});
+		                console.log("Sent " + elements1[i].href + " To https://urls.ajay.app/");
+			}
+		}
+
+	});
+}());
---
+++ readme.md
@@ -0,0 +1,13 @@
+Automatic Mediafire Link Submitter
+
+This is the repository for the Automatic Mediafire Link Submitter userscript/chrome extension.
+
+It automatically preserves all mediafire links on the pages you visit by submitting them [here](https://urls.ajay.app/).
+
+This script is based on [AS2WB](https://github.com/Zopolis4/AS2WB), credits for which are below.
+
+AS2WB Credits:</br>Original script author: Flare0n</br>
+Enhanced script author: ATRescue</br>This version: Zopolis4
+
+AMLA Credits:</br>
+Zopolis4
