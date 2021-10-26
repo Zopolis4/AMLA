a2583fe1fb8508349dd9fd561e57852d835c377e
Zopolis4 <creatorsmithmdt@gmail.com>
2021-10-26 15:44:15 +1100

    Update to support app.mediafire.com

--- AMLA/AMLA.js
+++ AMLA/AMLA.js
@@ -2,7 +2,7 @@
 // @name	Automatic Mediafire Link Archiver
 // @namespace      Zopolis4
 // @description    Automatically archives all mediafire links on pages you visit by sending them to ArchiveTeam.
-// @version	1.0.1
+// @version	1.1.0
 // @match	*
 // @run-at document-start
 // ==/UserScript==
@@ -10,24 +10,38 @@
 (function() {
 
 	document.addEventListener("DOMContentLoaded", function() {
+		var url = location.href
+		if (location.hostname == "app.mediafire.com") {
+		    url = encodeURI(decodeURI(location.href).replace(/(https?:\/\/)?app\.mediafire\.com\/?/, "https://www.mediafire.com/?"));
+                    fetch("https://urls.ajay.app/api/submitURLs", {
+                      method: "POST",
+                      body: JSON.stringify({
+                        urls: [ url ]
+                      }),
+                      headers: {
+                        "Content-Type": "application/json"
+                      }
+                    });
+                    console.log("Sent " + url + " To https://urls.ajay.app/");
+		}				
 		if (location.hostname == "www.mediafire.com") {
-                        fetch("https://urls.ajay.app/api/submitURLs", {
-			  method: "POST",
-			  body: JSON.stringify({
-			    urls: [ location.href ]
-			  }),
-			  headers: {
-                            "Content-Type": "application/json"
-			  }
-			});
-			console.log("Sent " + location.href + " To https://urls.ajay.app/");
+                      fetch("https://urls.ajay.app/api/submitURLs", {
+                        method: "POST",
+                        body: JSON.stringify({
+                          urls: [ url ]
+                        }),
+                        headers: {
+                          "Content-Type": "application/json"
+                        }
+                      });
+                      console.log("Sent " + url + " To https://urls.ajay.app/");
 		}
 	});
 
 
 
 	window.addEventListener("load", function() {
-		for (var elements1 = document.getElementsByTagName("a"), i = elements1.length - 1; i >= 0; i--) {
+		for (var elements1 = document.getElementsByTagName("a"), i = elements1.length - 1; i >= 0; i--) {		
 			if (elements1[i].hostname == "www.mediafire.com" || elements1[i].hostname == "www.mfi.re") {
 				fetch("https://urls.ajay.app/api/submitURLs", {
 				  method: "POST",
